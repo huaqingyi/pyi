ccf422bae4cabe3da5f98bd5e907622afb0b4e1c
huaqingyi <huaqingyi@yi.local>
2019-11-22 23:25:47 +0800

    update

--- dist/decorators/validation.d.ts
+++ dist/decorators/validation.d.ts
@@ -1,6 +1,7 @@
 import { PYICore } from '../core';
 import { ValidationError } from 'class-validator';
 export declare function Validation(target: any): any;
+export declare function IsNotEmptyIf(target: any & PYIValidation, key: string): any;
 export interface PYIValidationImpl {
     validate: () => any;
     throws: (errors: ValidationError[]) => any;
@@ -8,6 +9,7 @@ export interface PYIValidationImpl {
 export declare abstract class PYIValidation<Props = {}> extends PYICore implements PYIValidationImpl {
     static swaggerDocument: any;
     static _root(): typeof PYIValidation;
+    _ignore?: string[];
     validate(): Promise<ValidationError[]>;
     throws(errors: ValidationError[]): any;
 }
--- dist/decorators/validation.js
+++ dist/decorators/validation.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const lodash_1 = require("lodash");
 const core_1 = require("../core");
 const class_validator_1 = require("class-validator");
 const execption_1 = require("./execption");
@@ -10,12 +11,40 @@ function Validation(target) {
     return target;
 }
 exports.Validation = Validation;
+function IsNotEmptyIf(target, key) {
+    if (!target._ignore) {
+        target._ignore = [];
+    }
+    target._ignore.push(key);
+    return target._ignore;
+}
+exports.IsNotEmptyIf = IsNotEmptyIf;
 class PYIValidation extends core_1.PYICore {
     static _root() {
         return PYIValidation;
     }
     async validate() {
-        return await class_validator_1.validate(this);
+        return await class_validator_1.validate(this).then((errors) => {
+            // if (!this._ignore) { return errors; }
+            // return filter(errors, (error) => {
+            //     if ((this._ignore || []).indexOf(error.property) !== -1) {
+            //         if (validator.isEmpty(error.value)) {
+            //             return false;
+            //         }
+            //         return true;
+            //     }
+            //     return true;
+            // });
+            if (!this._ignore || this._ignore.length === 0) {
+                return errors;
+            }
+            return lodash_1.filter(errors, (err) => {
+                if ((this._ignore || []).indexOf(err.property) !== -1 && lodash_1.isEmpty(err.value)) {
+                    return false;
+                }
+                return true;
+            });
+        });
     }
     throws(errors) {
         return execption_1.PYIExecption(class {
